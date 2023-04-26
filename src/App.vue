ace982da72463cf5c6e2596adbd4126d8fd067dc
Adryan <adryan_freire@hotmail.com>
2023-04-26 16:15:52 -0300

    Relatório para alunos e professores

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <VariaveisFuncoes />
+  
+</template>
+
+<script>
+import VariaveisFuncoes from './components/VariaveisFuncoes.vue';
+
+export default {
+  components: {
+    VariaveisFuncoes
+  }
+}
+</script>
+
+<style>
+body{
+  background-color: aquamarine;
+}
+</style>
